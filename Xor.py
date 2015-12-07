edac620843a2cf234f6a4639286c778c42c14d68
salevin <samueljlevin@gmail.com>
2015-12-06 19:46:41 -0500

    Something something xor

--- Xor.py
+++ Xor.py
@@ -3,25 +3,30 @@ import numpy as np
 import tensorflow as tf
 
 if __name__ == '__main__':
-    x_data = np.array([[1.0, 1.0], [0.0, 0.0], [1.0, 0.0], [0.0, 1.0]], dtype=np.float32)
-    y_data = np.array([[-1.0], [-1.0], [1.0], [1.0]], dtype=np.float32)
+    x_data = np.array([[1.0, 1.0], [0.0, 0.0], [1.0, 0.0], [0.0, 1.0]],
+                      dtype=np.float32)
+    y_data = np.array([[-1.0], [-1.0], [1.0], [1.0]],
+                      dtype=np.float32)
 
-    hiddenDim = 2
-    # x = tf.placeholder(tf.float32, [4, 2])
-    # y_ = tf.placeholder(tf.float32, [4, 1])
+    hiddenDim = 4
 
-    b = tf.Variable(tf.zeros([hiddenDim]))
-    W = tf.Variable(tf.random_uniform([2, hiddenDim], -0.5, 0.5))
+    x = tf.placeholder(tf.float32, shape=[None, 2], name="x")
+    y_ = tf.placeholder(tf.float32, shape=[None, 1], name="y_")
+
+    b = tf.Variable(tf.zeros([hiddenDim]), name="b")
+    W = tf.Variable(tf.random_uniform([2, hiddenDim], -0.5, 0.5),
+                    name="W")
 
     b0 = tf.Variable(tf.zeros([1]))
     W0 = tf.Variable(tf.random_uniform([hiddenDim, 1], -0.5, 0.5))
 
-    hidden = tf.nn.softmax(tf.matmul(x_data, W) + b)
+    hidden = tf.nn.softmax(tf.matmul(x, W) + b)
 
     y = tf.nn.softmax(tf.matmul(hidden, W0) + b0)
 
-    loss = tf.reduce_mean(tf.square(y - y_data))
-    optimizer = tf.train.GradientDescentOptimizer(0.02)
+    entropy = tf.square(y_ - y)
+    loss = tf.reduce_mean(entropy)
+    optimizer = tf.train.AdamOptimizer(0.01)
     train = optimizer.minimize(loss)
 
     init = tf.initialize_all_variables()
@@ -31,8 +36,9 @@ if __name__ == '__main__':
 
     sess = tf.Session()
     sess.run(tf.initialize_all_variables())
-    with sess.as_default():
-        for step in xrange(0, 40001):
-            train.run()
-            if step % 500 == 0:
-                print step, loss.eval(x_data.all(), sess)
+
+    for epoch in xrange(0, 2001):
+        run = sess.run([train, loss, W, b, W, b0],
+                       feed_dict={x: x_data, y_: y_data})
+        if epoch % 400 == 0:
+            print epoch, run
